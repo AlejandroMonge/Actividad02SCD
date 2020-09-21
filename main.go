8c4926be98f9333d4ec7db7c0a77d84b72615b6b
AlejandroMonge <kmonge_rey@hotmail.com>
2020-09-21 12:04:35 -0500

    Mensaje mas largo

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	fmt.Println("Ahora se relaizo un cambio en el progrma")
+	fmt.Println("Por lo que se deberia crear un nuevo commit")
+	fmt.Println("Y guardar estos cambios realizados.")
 }
